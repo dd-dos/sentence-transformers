15dca99cafd6c5cfae1c9931c27d9a046841c19e
dd-dos <dungphamdang99@gmail.com>
2021-01-04 12:37:03 +0700

    fast commit

---
+++ scripts/train_sent_sim.sh
@@ -0,0 +1,3 @@
+python examples/training/sentences_similarity/train.py \
+        --data_path /content/drive/MyDrive/training_data/jap_sentences_data.zip \
+        --save_path /content/drive/MyDrive/training/Sentence-similarity
