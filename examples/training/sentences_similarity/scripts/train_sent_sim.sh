28c230a60d5f15501f2b9bdcd18478faaa29f840
dd-dos <dungphamdang99@gmail.com>
2021-03-04 13:32:21 +0700

    fast commit

--- examples/training/sentences_similarity/scripts/train_sent_sim.sh
+++ examples/training/sentences_similarity/scripts/train_sent_sim.sh
@@ -1,2 +1,2 @@
-python sentences_similarity/train.py --data_path /content/drive/MyDrive/training_data/sentence_similarity/relevance_score_data.tsv.gz \
+python train.py --data_path /content/drive/MyDrive/training_data/sentence_similarity/relevance_score_data.tsv.gz \
                                                        --save_path /content/drive/MyDrive/training/Sentence-similarity
